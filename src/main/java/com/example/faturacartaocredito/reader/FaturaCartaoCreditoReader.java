3dd858ed8f9b53621850e5b9786390017a0dea4e
Jauri da Cruz Junior <jauriads@gmail.com>
2021-04-22 15:25:48 -0300

    Implementação da operação de leitura

---
+++ src/main/java/com/example/faturacartaocredito/reader/FaturaCartaoCreditoReader.java
@@ -0,0 +1,81 @@
+package com.example.faturacartaocredito.reader;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+import com.example.faturacartaocredito.domain.FaturaCartaoCredito;
+import com.example.faturacartaocredito.domain.Transacao;
+
+public class FaturaCartaoCreditoReader implements ItemStreamReader<FaturaCartaoCredito>{
+	
+	private ItemStreamReader<Transacao> delegate;
+	private Transacao transacaoAtual;
+	
+	
+
+	public FaturaCartaoCreditoReader(ItemStreamReader<Transacao> delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public void open(ExecutionContext executionContext) throws ItemStreamException {
+		delegate.open(executionContext);
+		
+	}
+
+	@Override
+	public void update(ExecutionContext executionContext) throws ItemStreamException {
+		delegate.update(executionContext);
+		
+	}
+
+	@Override
+	public void close() throws ItemStreamException {
+		delegate.close();
+		
+	}
+
+	@Override
+	public FaturaCartaoCredito read()
+			throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+
+		if (transacaoAtual == null) {
+			transacaoAtual = delegate.read();
+		}
+		
+		FaturaCartaoCredito faturaCartaoCredito = null;
+		Transacao transacao = transacaoAtual;
+		transacaoAtual = null;
+		
+		if (transacao != null) {
+			faturaCartaoCredito = new FaturaCartaoCredito();
+			faturaCartaoCredito.setCartaoCredito(transacao.getCartaoCredito());
+			faturaCartaoCredito.setCliente(transacao.getCartaoCredito().getCliente());
+			faturaCartaoCredito.getTransacaos().add(transacao);
+			
+			while(isTransacaoRelacionada(transacao)) {
+				faturaCartaoCredito.getTransacaos().add(transacaoAtual);
+				
+			}
+			
+		}
+		
+		
+
+		return faturaCartaoCredito;
+	}
+
+	private boolean isTransacaoRelacionada(Transacao transacao) throws Exception {
+		return peek() != null && transacao.getCartaoCredito().getNumeroCartaoCredito() == transacaoAtual.getCartaoCredito().getNumeroCartaoCredito();
+	}
+
+	private Transacao peek() throws Exception {
+		transacaoAtual = delegate.read();
+		return transacaoAtual;
+	}
+
+}
--- src/main/java/com/example/faturacartaocredito/step/FaturaCartaoCreditoStepConfig.java
+++ src/main/java/com/example/faturacartaocredito/step/FaturaCartaoCreditoStepConfig.java
@@ -3,13 +3,15 @@ package com.example.faturacartaocredito.step;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.faturacartaocredito.domain.FaturaCartaoCredito;
+import com.example.faturacartaocredito.domain.Transacao;
+import com.example.faturacartaocredito.reader.FaturaCartaoCreditoReader;
 
 @Configuration
 public class FaturaCartaoCreditoStepConfig {
@@ -19,13 +21,13 @@ public class FaturaCartaoCreditoStepConfig {
 	
 	@Bean
 	public Step faturaCartaoCreditoStep(
-			ItemReader<FaturaCartaoCredito> lerTRansacoesReader,
+			ItemStreamReader<Transacao> lerTransacoesReader,
 			ItemProcessor<FaturaCartaoCredito, FaturaCartaoCredito> carregaDadosClienteProcessor,
 			ItemWriter<FaturaCartaoCredito> escreverFaturaCartaoCredito) {
 		return stepBuilderFactory
 				.get("faturaCartaoCreditoStep")
 				.<FaturaCartaoCredito, FaturaCartaoCredito>chunk(1)
-				.reader(lerTRansacoesReader)
+				.reader(new FaturaCartaoCreditoReader(lerTransacoesReader))
 				.processor(carregaDadosClienteProcessor)
 				.writer(escreverFaturaCartaoCredito)
 				.build();
